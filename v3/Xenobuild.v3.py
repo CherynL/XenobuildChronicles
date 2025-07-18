7cd407eee93704ad70730cbdd62f564b75483365
Cheryn <112348692+CherynL@users.noreply.github.com>
2025-07-17 22:34:28 -0500

    Fully Implemented v3
    Added levels
    Added Weapon selection
    - Weapons clarify if they can damage the Mechon, Faces, or both
    - Monado damage scales with level

--- v3/Xenobuild.v3.py
+++ v3/Xenobuild.v3.py
@@ -17,48 +17,40 @@ from tkinter import ttk
 #       Automatically selects highest rank and value for gems
 
 # v3 features:
-#   Add level slider
+#   Added levels
+#   Added Weapon selection
+#       Weapons clarify if they can damage Mechon and Faces
+#       Monado damage scales with level
 
 # features to add:
-#   Add frames
-#   Weapons and attack
-#   Level and base stats
+#   Auto-attack
+#   Base stats
+#   Gem Calculations
 #   Extra gem slot options
 #   skill trees and skill linking
 #   Affinity coins for skill linking
 
+# ----------
 # Excel File
+# ----------
+
 XenobuildData = 'Xenobuild.v3.xlsx'
 # Sheet Names
-CS = "Characters" # as in Character Select
+# Character Stat Tables not Included as Variables
 HE = "Head_Equip"
 TE = "Torso_Equip"
 AE = "Arm_Equip"
 LE = "Leg_Equip"
 FE = "Foot_Equip"
-#GL = "Gems_Linked"
 GU = "Gems_Unlinked" # Unlinked, Manually edited
-#GR = "Gems_ByRank" # Includes used for minimum and maximum
-#GV = "Gems_Values" # List of all possible gem values
 GB = "Gems_Bestest" # Only Rank VI Gems
-#WS = "Weapons_Source"
 WU = "Weapons_Unlinked"
 
-
-
-# Parameters
-GS = 2 # Gem Slot Column
-PD = GS+3 # Because the Gems can take up a lot of space
-WP = 4 # Weapon Row offset
-
 # Gem Settings:
-gem_options = ['Bestest', 'Best', 'Full', 'Worst', 'Worstest'] # TODO Add functionality for this.
+gem_options = ['Bestest', 'Best', 'Full', 'Worst', 'Worstest'] # TODO Add functionality for this. (After the skill trees I think)
 gem_mode = 'Bestest'
 
 # Methods
-# Character Dataframe
-CS_df = pd.read_excel(XenobuildData, sheet_name=CS)
-CS_df.set_index('Name', inplace=True)
 # Gem Dataframe
 GU_df = pd.read_excel(XenobuildData, sheet_name=GU)
 GU_df.set_index('Gem', inplace=True)
@@ -69,18 +61,34 @@ GB_df.set_index('Gem', inplace=True)
 WU_df = pd.read_excel(XenobuildData, sheet_name=WU)
 WU_df.set_index('Name', inplace=True)
 
-'''
-class Character:
+# Grid Parameters
+GS = 2 # Gem Slot Column
+PD = GS+3 # Because the Gems can take up a lot of space
+WP = 5 # Weapon Row offset
+
+# -------
+# Classes
+# -------
+
+class Character: # These values are NOT tkinter widgets
     
-    def __init__ (self, name = None, min_level = None, hp = None, strength = None, ether = None, agility = None):
+    def __init__ (
+            self, name = None, hp = 100, strength = 1, ether = 1, agility = 1,
+            dmg_min = 1, dmg_max = 1, crit = 0, p_def = 0, e_def = 0, block = 0
+            ):
         self.name = name
-        self.min_level = min_level
         self.hp = hp
         self.strength = strength
         self.ether = ether
         self.agility = agility
+        self.dmg_min = dmg_min
+        self.dmg_max = dmg_max
+        self.crit = crit
+        self.p_def = p_def
+        self.e_def = e_def
+        self.block = block
 
-'''
+
 
 
 # Gem Class
@@ -133,6 +141,7 @@ class Character_Build:
         self.leg = leg
         self.foot = foot
 """
+character = Character()
 weapon = Weapon()
 head = Equipment('Head', HE)
 torso = Equipment('Torso',TE)
@@ -140,15 +149,24 @@ arm = Equipment('Arm', AE)
 leg = Equipment('Leg', LE)
 foot = Equipment('Foot', FE)
 equips = [head, torso, arm, leg, foot]
+characters = ['Shulk','Reyn','Fiora','Dunban','Sharla','Riki','Melia','Seven']
+
+# ---------------
+# Bound Functions
+# ---------------
+
 # Updating for equippable armour
 def update_items(*args):
     selected_character = character_combobox.get() # update valid armour
-    weapon.combobox.set("Select...") #reset
-    weapon.combobox["values"] = WU_df[WU_df[selected_character]].index.tolist()
-    weapon.gem0.combobox.set("Unslotted") #reset
-    weapon.gem1.combobox.set("Unslotted") #reset
-    weapon.gem2.combobox.set("Unslotted") #reset
-
+    CS_df = pd.read_excel(XenobuildData, sheet_name=selected_character)
+    CS_df.set_index('Level', inplace=True)
+    level_combobox["values"] = CS_df.index.tolist()
+    level_combobox.set("99") #reset
+    for g in [weapon.gem0,weapon.gem1,weapon.gem2]:
+        g.combobox.set("Unslotted")
+    default_weapon = WU_df[WU_df[selected_character]].index.tolist()
+    weapon.combobox["values"] = default_weapon
+    weapon.combobox.set(default_weapon[0])
     equips = [head, torso, arm, leg, foot]
     # Making 'Name' the index
     for i in equips:
@@ -158,8 +176,9 @@ def update_items(*args):
         i.combobox.set("Not Equipped") #reset
         i.gem.combobox.set("Unslotted") #reset
 
+
 # Updating Stats and slots
-def update_equip_stats(*args):
+def update_equips(*args):
     equips = [head, torso, arm, leg, foot] # I wonder if there's an easier way to bulk-assign values in python
     for i in equips:
         df = pd.read_excel(XenobuildData, sheet_name=i.sheet_name)
@@ -191,68 +210,91 @@ def update_equip_stats(*args):
         i.p_def.config(   text=df.at[i.combobox.get(),  'Phy_Def'])
         i.e_def.config(   text=df.at[i.combobox.get(),  'Eth_Def'])
         i.weight.config(  text=df.at[i.combobox.get(),  'Weight'])
-    stats_totals = [
-        (total_p_def, equip_p_defs),
-        (total_e_def, equip_e_defs),
-        (total_weight, equip_weights)
-    ]
-    for total, equip in stats_totals:
-        stat_sum=sum([int(w["text"]) for w in equip])
-        total.config(text=stat_sum)
 
-def update_weapon_stats(*args): # TODO WORK ON THIS SECTION
+    update_totals()
+
     
-    for i, gem in enumerate(weapon.gems):
-        # Gem Slot selection when equipment is chosen
-        gem_type = WU_df.at[gem.combobox.get(),  'Gem.'+i]
+
+def update_weapon(*args): # TODO WORK ON THIS SECTION
+    
+    for i, gem in enumerate([weapon.gem0, weapon.gem1, weapon.gem2]):
+        # Gem Slot selection when equipment is 
+        gem_type = WU_df.at[weapon.combobox.get(),  f"Gem.{i}"]
         match gem_type:
             case 'Open':
                 gem.combobox["values"] = GB_df[GB_df['Equipment'].isin(['Weapon', 'All'])].index.tolist()[1:]
-                if gem.combobox.get() == gem_type or gem.combobox.get() == 'Unslotted': gem.combobox.set("Open")
-                gem.rank.config(text = GB_df.at[gem.combobox.get(), 'Rank.'+i])
-                gem.value.config(text = GB_df.at[gem.combobox.get(), 'Value.'+i])
+                if gem.combobox.get() == gem_type or gem.combobox.get() == 'Unslotted': gem.combobox.set('Open')
+                gem.rank.config(text = GB_df.at[gem.combobox.get(), 'Rank'])
+                gem.value.config(text = GB_df.at[gem.combobox.get(), 'Value'])
             case _: # works for uniques and no-slots
                 gem.combobox["values"] = []
                 gem.combobox.set(gem_type)
-                gem.rank.config(text = WU_df.at[gem.combobox.get(), 'Rank.'+i])
-                gem.value.config(text = WU_df.at[gem.combobox.get(), 'Value.'+i])
-        
-    '''
-        # The rest of the stats
-        i.p_def.config(   text=df.at[i.combobox.get(),  'Phy_Def'])
-        i.e_def.config(   text=df.at[i.combobox.get(),  'Eth_Def'])
-        i.weight.config(  text=df.at[i.combobox.get(),  'Weight'])
-    stats_totals = [
-        (total_p_def, equip_p_defs),
-        (total_e_def, equip_e_defs),
-        (total_weight, equip_weights)
-    ]
-    for total, equip in stats_totals:
-        stat_sum=sum([int(w["text"]) for w in equip])
-        total.config(text=stat_sum)
-    '''
+                gem.rank.config(text = WU_df.at[weapon.combobox.get(), f'Rank.{i}'])
+                gem.value.config(text = WU_df.at[weapon.combobox.get(), f'Value.{i}'])
+    # Weapon Stats
+    misc_flag = WU_df.at[weapon.combobox.get(), "Misc_Flag"]
+    if(misc_flag >= 3): anti_mechon_flag.config(text="YES")
+    else: anti_mechon_flag.config(text="NO")
+    if(misc_flag >= 5): min_mod = int(level_combobox.get())*1.4; max_mod = int(level_combobox.get())*1.5
+    else: min_mod = max_mod = 1
+    if(misc_flag >= 7): unshackled_flag.config(text='YES')
+    else: unshackled_flag.config(text='NO')
+
+    weapon.dmg_min.config(text=min(int(WU_df.at[weapon.combobox.get(), 'Dmg_Min']*min_mod),999))
+    weapon.dmg_max.config(text=min(int(WU_df.at[weapon.combobox.get(), 'Dmg_Max']*max_mod),999))
+    weapon.crit.config(text=WU_df.at[weapon.combobox.get(), 'Crit'])
+    weapon.p_def.config(text=WU_df.at[weapon.combobox.get(), 'Phy_Def'])
+    weapon.e_def.config(text=WU_df.at[weapon.combobox.get(), 'Eth_Def'])
+    weapon.block.config(text=WU_df.at[weapon.combobox.get(), 'Block'])
+
+    update_totals()
+
+
+def update_totals():
+    # Defenses (NO GEMS)
+    p_def_sum = sum([int(c["text"]) for c in equip_p_defs])+int(weapon.p_def["text"])+character.p_def
+    total_p_def.config(text=p_def_sum)
+    e_def_sum = sum([int(c["text"]) for c in equip_e_defs])+int(weapon.e_def["text"])+character.e_def
+    total_e_def.config(text=e_def_sum)
+    
+    # Weight
+    weight_sum = sum([int(c["text"]) for c in equip_weights])
+    total_weight.config(text=weight_sum)
+
+def update_stats():
+    CS_df = pd.read_excel(XenobuildData, sheet_name=character_combobox.get())
+    CS_df.set_index('Level', inplace=True)
+    level = level_combobox.get()
+    hp = CS_df.at[level,'HP']
+    strength = CS_df.at[level,'Strength']
+    ether = CS_df.at[level,'Ether']
+    agility = CS_df.at[level,'Agility']
+
+# ------
+# WINDOW
+# ------
 
 # Starting the Window
 root = tk.Tk()
 root.title("Xenobuild Chronicles")
 # Character Dropdown
-character_frame = tk.Frame(root)
+character_frame = tk.Frame(root, borderwidth=5,relief='groove')
 character_frame.grid(row=0, column=0, sticky="W")
 #character_label = ttk.Label(character_frame, text="Character")
 #character_label.grid(row=0, column=0, padx=5, pady=5)
 ttk.Label(character_frame, text="Character").grid(row=0, column=0, padx=5, pady=5)
-character_combobox = ttk.Combobox(character_frame, values=list(CS_df.index),state='readonly',width=8)
+character_combobox = ttk.Combobox(character_frame, values=characters,state='readonly',width=8)
 character_combobox.set("Select...")
 character_combobox.grid(row=0, column=1, padx=5, pady=5)
 # Level Dropdown
 ttk.Label(character_frame, text="Lv").grid(row=0, column=2, padx=5, pady=5)
-level_combobox = ttk.Combobox(character_frame, values=[i for i in range(1,100)],state='readonly',width=3)
-level_combobox.set("99")
+level_combobox = ttk.Combobox(character_frame, values=[],state='readonly',width=3)
 level_combobox.grid(row=0, column=3, padx=5, pady=5)
 
-# Equips Frame
-equips_frame = tk.Frame(root)
+# Equipment Frame
+equips_frame = tk.Frame(root,borderwidth=5,relief='groove')
 equips_frame.grid(row=1, column=0)
+
 
 # Labels
 ttk.Label(equips_frame, text='Equipment').grid(row=0, column=1, padx=5, pady=5)
@@ -268,6 +310,20 @@ ttk.Label(equips_frame, text='Value').grid(row=WP, column=GS+2, padx=5, pady=5) 
 ttk.Label(equips_frame, text='Weapon').grid(row=1, column=0, padx=5, pady=5)
 weapon.combobox = ttk.Combobox(equips_frame, state='readonly',width=18)
 weapon.combobox.grid(row=1, column=1, padx=5, pady=5)
+
+# Misc_Flag Frame
+misc_flag_frame = tk.Frame(equips_frame)
+misc_flag_frame.grid(row=2,column=0,rowspan=2,columnspan=2, sticky='W')
+ttk.Label(misc_flag_frame, text='Anti-Mechon').grid(row=0,column=0, padx=5, pady=5)
+ttk.Label(misc_flag_frame, text='Unshackled').grid(row=1,column=0, padx=5, pady=5)
+anti_mechon = ttk.Label(misc_flag_frame, text='Anti-Mechon:')
+anti_mechon.grid(row=0,column=0, padx=5, pady=5)
+anti_mechon_flag = ttk.Label(misc_flag_frame, text='Y/N')
+anti_mechon_flag.grid(row=0,column=1, padx=5, pady=5)
+unshackled = ttk.Label(misc_flag_frame, text='Unshackled:')
+unshackled.grid(row=1,column=0, padx=5, pady=5)
+unshackled_flag = ttk.Label(misc_flag_frame, text='Y/N')
+unshackled_flag.grid(row=1,column=1, padx=5, pady=5)
 
 # Weapon Stats
 weapon_stats = []
@@ -304,13 +360,9 @@ for i, label_name in enumerate(['P Def', 'E Def', 'Block']):
     stat.grid(row=3, column=PD+i, padx=5, pady=5)
     weapon_stats.append((stat))
 
-weapon.p_def, weapon.e_def, weapon.crit, weapon.p_def, weapon.e_def, weapon.block = [w for w in weapon_stats]
+weapon.dmg_min, weapon.dmg_max, weapon.crit, weapon.p_def, weapon.e_def, weapon.block = [w for w in weapon_stats]
 
-#   Empty,  Equipment,  [Gems], Dmg_Min,    Dmg_Max,    Crit
-#   Weapon, Combobox,
-#   Anti-Mechon?,   Y/N,        Phy_Def,    Eth_Def,    Block
-#   Unshackled?,    Y/N,        
-
+ttk.Separator(equips_frame,orient='horizontal').grid(row=WP-1,column=0,columnspan=8,sticky='ew',padx=5,pady=5)
 
 # Equips Dropdown
 equip_comboboxes = []
@@ -366,7 +418,9 @@ head.p_def,         torso.p_def,        arm.p_def,          leg.p_def,          
 head.e_def,         torso.e_def,        arm.e_def,          leg.e_def,          foot.e_def          = [w for w in equip_e_defs]
 head.weight,        torso.weight,       arm.weight,         leg.weight,         foot.weight         = [w for w in equip_weights]
 
-
+# -----------
+# STAT TOTALS
+# -----------
 
 # Defensive Stat Totals
 ttk.Label(equips_frame, text="Total").grid(row=6+WP, column=PD-1, padx=5, pady=5)
@@ -377,15 +431,44 @@ for i, label_name in enumerate(['P Def','E Def','Weight']):
     stat_totals.append((total))
 
 total_p_def, total_e_def, total_weight = [w for w in stat_totals]
+'''
+# ---------------
+# Character Stats
+# ---------------
+
+stat_frame = tk.Frame(root, borderwidth=5, relief='groove')
+stat_frame.grid(row=0, column=1,rowspan=2,padx=5,pady=5)
+ttk.Label(stat_frame,text="HP").grid(row=0,column=0,padx=5,pady=5)
+ttk.Label(stat_frame,text="Strength").grid(row=1,column=0,padx=5,pady=5)
+ttk.Label(stat_frame,text="Ether").grid(row=2,column=0,padx=5,pady=5)
+ttk.Label(stat_frame,text="Agility").grid(row=3,column=0,padx=5,pady=5)
+
+hp = ttk.Label(stat_frame,text="–")
+hp.grid(row=0,column=1,padx=5,pady=5)
+strength = ttk.Label(stat_frame,text="–")
+strength.grid(row=1,column=1,padx=5,pady=5)
+ether = ttk.Label(stat_frame,text="–")
+ether.grid(row=2,column=1,padx=5,pady=5)
+agility = ttk.Label(stat_frame,text="–")
+agility.grid(row=3,column=1,padx=5,pady=5)
+'''
+# -------
+# BINDING
+# -------
 
 # Binding the equip options for Characters
 character_combobox.bind("<<ComboboxSelected>>", update_items)
+character_combobox.bind("<<ComboboxSelected>>", update_weapon, add='+')
 # Binding stats for the Level
-level_combobox.bind("<<ComboboxSelected>>", update_equip_stats)
+level_combobox.bind("<<ComboboxSelected>>", update_weapon)
+level_combobox.bind("<<ComboboxSelected>>", update_equips)
 # Binding stats for Equips
-for w in equip_comboboxes:  w.bind("<<ComboboxSelected>>", update_equip_stats)
+for w in equip_comboboxes: w.bind("<<ComboboxSelected>>", update_equips)
 # Binding for gems
-for w in equip_gems: w.bind("<<ComboboxSelected>>", update_equip_stats)
+for w in equip_gems: w.bind("<<ComboboxSelected>>", update_equips)
+
+weapon.combobox.bind("<<ComboboxSelected>>", update_weapon)
+for w in weapon_gems: w.bind("<<ComboboxSelected>>", update_weapon)
 
 # Happens at the end
 root.mainloop()
